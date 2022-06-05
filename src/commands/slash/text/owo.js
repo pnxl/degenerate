98457a3d99d744babc69d294e999ec007e3d58fb
jason <pix3l.icious@gmail.com>
2022-06-05 14:50:18 +0700

    owo slash

---
+++ src/commands/slash/text/owo.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const transform = require("../../../misc/helpers/transform.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("owo")
+    .setDescription("owo-ifies the given string")
+    .addStringOption((option) =>
+      option
+        .setName("string")
+        .setDescription("enter a message you want to owo-ify")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const str = interaction.options.getString("message");
+
+    const transformed = transform.owo(str);
+
+    const av = await interaction.user.displayAvatarURL({
+      dynamic: true,
+    });
+
+    const webhook = await interaction.channel.createWebhook(
+      interaction.member.displayName,
+      { avatarURL: avatar }
+    );
+
+    webhook.send({
+      content: transformed,
+      username: interaction.member.displayName,
+      avatarURL: av,
+    });
+
+    await interaction.reply({
+      content: "Webhook sent!",
+      ephemeral: true,
+    });
+
+    await webhook.delete();
+  },
+};
