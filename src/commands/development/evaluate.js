acc54ef8402d98b37786111d3f83091a601a468f
jason <pix3l.icious@gmail.com>
2022-06-06 20:37:54 +0700

    completely forgot about this command

--- src/commands/development/evaluate.js
+++ src/commands/development/evaluate.js
@@ -1,40 +1,31 @@
 // import modules
+const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
 const print = require("../../misc/helpers/print.js");
 const cfg = require("../../../cfg.json");
 const { inspect } = require("util");
 
 module.exports = {
-  name: "evaluate",
-  aliases: ["eval", "ev", "js", "exec", "ex", "code", "execute"],
-  hidden: true,
-  async execute(message, args) {
-    // if no args and not jason spit error in console
+  data: new SlashCommandBuilder()
+    .setName("evaluate")
+    .setDescription("evaluates the given string of code")
+    .addStringOption((option) =>
+      option
+        .setName("code")
+        .setDescription("enter the code to evaluate!")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
     if (
-      (!args.length && message.author.id !== cfg.info.owner.id[0]) ||
-      (!args.length && message.author.id !== cfg.info.owner.id[1])
-    ) {
-      message.delete();
-      print.error(
-        "A user attempted to run evaluate! Make sure to remove evaluate before getting Penguino ready for production!"
-      );
-      return message.reply("You did not provide any arguments!").then((m) => {
-        setTimeout(() => m.delete(), 2500);
-      });
-    }
-
-    if (
-      message.author.id === cfg.info.owner.id[0] ||
-      message.author.id === cfg.info.owner.id[1]
+      interaction.author.id === cfg.info.owner.id[0] ||
+      interaction.author.id === cfg.info.owner.id[1]
     ) {
       // actual eval code
       print.warn(
-        `'${message.author.tag}' ran evaluate at <#${
-          message.channel.id
+        `'${interaction.author.tag}' ran evaluate at <#${
+          interaction.channel.id
         }> and ran:\n            ${args.join(" ")}`
       );
-
-      message.delete();
 
       const snippet = args.join(" ");
 
@@ -52,27 +43,22 @@ module.exports = {
           iconURL: message.author.displayAvatarURL(),
         })
         .setColor(cfg.embed.colour)
-        .setTitle("Evaluation")
-        .addField("Code snippet", `\`\`\`js\n${snippet.slice(0, 1015)}\`\`\``)
-        .addField("Node.js results", `\`\`\`js\n${res.slice(0, 1015)}\`\`\``)
+        .setTitle("evaluation")
+        .addField("code snippet", `\`\`\`js\n${snippet.slice(0, 1015)}\`\`\``)
+        .addField("node.js results", `\`\`\`js\n${res.slice(0, 1015)}\`\`\``)
         .setFooter({
-          text: `Running Node.js ${process.version}`,
+          text: `running node.js ${process.version}`,
         })
         .setTimestamp();
 
-      message.channel.send({ embeds: [embed] }).then((m) => {
-        setTimeout(() => m.delete(), 5000);
-      });
+      interaction.reply({ embeds: [embed], ephemeral: true });
     } else {
-      // if not jason spit error in console
-      message.delete();
-      message.channel
-        .send("You do not have permission to use that command!")
-        .then((m) => {
-          setTimeout(() => m.delete(), 2500);
-        });
+      interaction.reply({
+        content: "you do not have permission to use that command!",
+        ephemeral: true,
+      });
       print.error(
-        "A user attempted to run evaluate! Make sure to remove evaluate before getting Penguino ready for production!"
+        "A user attempted to run evaluate! Make sure to remove evaluate before getting ready for production!"
       );
     }
   },
