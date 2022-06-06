268865d43f5b390fbbce0b9d9d41d0461a14d384
jason <pix3l.icious@gmail.com>
2022-06-06 21:16:08 +0700

    biased bot - i told kei i was going to sleep but im coding

--- src/commands/fun/ship.js
+++ src/commands/fun/ship.js
@@ -27,54 +27,89 @@ module.exports = {
     const user2slice = user2.slice(user2.length / 2);
     const percent = Math.floor(Math.random() * 100) + 1;
 
+    function percentageBias() {
+      if (
+        (interaction.options.getUser("user1")?.id === "648896419020668928" &&
+          interaction.options.getUser("user2")?.id === "466453424930553856") ||
+        (interaction.options.getUser("user2")?.id === "648896419020668928" &&
+          interaction.options.getUser("user1")?.id === "466453424930553856")
+      ) {
+        return 100;
+      } else if (
+        (interaction.options.getUser("user1")?.id === "967724967741513808" &&
+          interaction.options.getUser("user2")?.id === "763970485373370370") ||
+        (interaction.options.getUser("user2")?.id === "967724967741513808" &&
+          interaction.options.getUser("user1")?.id === "763970485373370370")
+      ) {
+        return 100;
+      } else if (
+        (interaction.options.getUser("user1")?.id === "728180364962562108" &&
+          interaction.options.getUser("user2")?.id === "775550130115444776") ||
+        (interaction.options.getUser("user2")?.id === "728180364962562108" &&
+          interaction.options.getUser("user1")?.id === "775550130115444776")
+      ) {
+        return 100;
+      } else {
+        return percent;
+      }
+    }
+
+    function shipName() {
+      if (
+        (interaction.options.getUser("user1")?.id === "967724967741513808" &&
+          interaction.options.getUser("user2")?.id === "763970485373370370") ||
+        (interaction.options.getUser("user2")?.id === "967724967741513808" &&
+          interaction.options.getUser("user1")?.id === "763970485373370370")
+      ) {
+        return "keison";
+      } else {
+        return user1slice + user2slice;
+      }
+    }
+
     const baseEmbed = new MessageEmbed()
       .setTitle(`this isn't going to work out... 😬`)
       .setColor(cfg.embed.colours.default)
-      .setDescription(`**${percent}%** | nothing much to say here...`);
+      .setDescription(`**${percentageBias()}%** | nothing much to say here...`);
 
     const thirtyEmbed = new MessageEmbed()
       .setTitle(`cute couple 😅`)
       .setColor(cfg.embed.colours.default)
-      .setDescription(`**${percent}%** | not much to say here...`);
+      .setDescription(`**${percentageBias()}%** | not much to say here...`);
 
     const fiftyEmbed = new MessageEmbed()
       .setTitle(`adorable couple!! 😊`)
       .setColor(cfg.embed.colours.default)
       .setDescription(
-        `**${percent}%** | i call this ship "${user1slice + user2slice}"`
+        `**${percentageBias()}%** | i call this ship "${shipName()}"`
       );
 
     const eightyEmbed = new MessageEmbed()
       .setTitle(`AHH CUTE!!~ 😍`)
       .setColor(cfg.embed.colours.default)
       .setDescription(
-        `**${percent}%** | i call this ship "${user1slice + user2slice}"`
+        `**${percentageBias()}%** | i call this ship "${shipName()}"`
       );
 
     const topEmbed = new MessageEmbed()
-      .setTitle(`${user1} + ${user2} = ${user1slice + user2slice}`)
+      .setTitle(`a match made in heaven. 💖`)
       .setColor(cfg.embed.colours.default)
-      .setDescription(`**${percent}%** | a match made in heaven. 💖`);
+      .setDescription(
+        `**${percentageBias()}%** | i call this ship "${shipName()}"`
+      );
 
-    switch (percent) {
-      case percent < 30:
-        interaction.reply({ embeds: [baseEmbed] });
-        break;
-      case percent < 50:
-        interaction.reply({ embeds: [thirtyEmbed] });
-        break;
-      case percent < 80:
-        interaction.reply({ embeds: [fiftyEmbed] });
-        break;
-      case percent < 95:
-        interaction.reply({ embeds: [eightyEmbed] });
-        break;
-      case percent < 98:
-        interaction.reply({ embeds: [topEmbed] });
-        break;
-      default:
-        interaction.reply({ embeds: [fiftyEmbed] });
-        break;
+    if (percentageBias() < 30) {
+      interaction.reply({ embeds: [baseEmbed] });
+    } else if (percentageBias() < 50) {
+      interaction.reply({ embeds: [thirtyEmbed] });
+    } else if (percentageBias() < 80) {
+      interaction.reply({ embeds: [fiftyEmbed] });
+    } else if (percentageBias() < 95) {
+      interaction.reply({ embeds: [eightyEmbed] });
+    } else if (percentageBias() > 98) {
+      interaction.reply({ embeds: [topEmbed] });
+    } else {
+      interaction.reply({ embeds: [fiftyEmbed] });
     }
   },
 };
