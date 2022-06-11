0588071713df7cd088b5fe59b899cd00a1fed69e
Jason <pix3l.icious@gmail.com>
2022-06-12 05:34:33 +0700

    add unknown values

--- src/commands/information/statistics.js
+++ src/commands/information/statistics.js
@@ -70,9 +70,9 @@ module.exports = {
       cpu: Math.round((await os.currentLoad()).currentLoad),
       mem: Math.floor(
         100 -
-        (Math.round((await os.mem()).free / 1073741824) /
-          Math.round(hostinfo.mem.info.total / 1073741824)) *
-        100
+          (Math.round((await os.mem()).free / 1073741824) /
+            Math.round(hostinfo.mem.info.total / 1073741824)) *
+            100
       ),
     };
 
@@ -142,120 +142,224 @@ module.exports = {
       if (hostinfo.sys.manufacturer === "Acidanthera") return true;
 
       return false;
-    };
+    }
 
     function bootMode() {
-      const parsedBoot = hostinfo.os.sys.uefi.toString()
-        .replace('true', 'UEFI')
-        .replace('false', 'CSM/Legacy')
-      
-      if (hostinfo.os.sys.distro === "macOS" || hostinfo.os.sys.distro === "Mac OS X") return parsedBoot.replace('UEFI', 'Apple EFI')
-      return parsedBoot
+      const parsedBoot = hostinfo.os.sys.uefi
+        .toString()
+        .replace("true", "UEFI")
+        .replace("false", "CSM/Legacy");
+
+      if (
+        hostinfo.os.sys.distro === "macOS" ||
+        hostinfo.os.sys.distro === "Mac OS X"
+      )
+        return parsedBoot.replace("UEFI", "Apple EFI");
+      return parsedBoot;
     }
 
     const board = new MessageEmbed()
       .setTitle(`🖥 system`)
       .setColor(cfg.embed.colours.default)
-      .addField("manufacturer", hostinfo.sys.manufacturer, true)
-      .addField("model", hostinfo.sys.model, true)
-      .addField("stock unit", hostinfo.sys.sku, true)
-      .addField("revision", hostinfo.sys.version, true)
-      .addField("virtualised", hostinfo.sys.virtual.toString().replace("true", "yes").replace("false", "no"), true)
-      .addField("hackintosh", hackintosh().toString().replace("true", "yes").replace("false", "no"), true)
+      .addField("manufacturer", hostinfo.sys.manufacturer || "???", true)
+      .addField("model", hostinfo.sys.model || "???", true)
+      .addField("stock unit", hostinfo.sys.sku || "???", true)
+      .addField("revision", hostinfo.sys.version || "???", true)
+      .addField(
+        "virtualised",
+        hostinfo.sys.virtual
+          .toString()
+          .replace("true", "yes")
+          .replace("false", "no"),
+        true
+      )
+      .addField(
+        "hackintosh",
+        hackintosh().toString().replace("true", "yes").replace("false", "no"),
+        true
+      );
 
     const cpu = new MessageEmbed()
       .setTitle(`🦋 processor`)
       .setColor(cfg.embed.colours.default)
-      .addField("manufacturer", hostinfo.cpu.manufacturer, true)
-      .addField("vendor", hostinfo.cpu.vendor, true)
-      .addField("brand", hostinfo.cpu.brand, true)
-      .addField("family", hostinfo.cpu.family, true)
-      .addField("model", hostinfo.cpu.model, true)
-      .addField("stepping", hostinfo.cpu.stepping, true)
-      .addField("base clock", hostinfo.cpu.speed + " GHz", true)
-      .addField("min speed", hostinfo.cpu.speedMin + " GHz", true)
-      .addField("max speed", hostinfo.cpu.speedMax + " GHz", true)
-      .addField("num of CPUs", hostinfo.cpu.processors.toString(), true)
-      .addField("cores", hostinfo.cpu.physicalCores.toString(), true)
-      .addField("threads", hostinfo.cpu.cores.toString(), true)
-      .addField("supported features", "```\n" + hostinfo.cpu.flags.replaceAll(' ', ', ') + "\n```")
-    
+      .addField("manufacturer", hostinfo.cpu.manufacturer || "???", true)
+      .addField("vendor", hostinfo.cpu.vendor || "???", true)
+      .addField("brand", hostinfo.cpu.brand || "???", true)
+      .addField("family", hostinfo.cpu.family || "???", true)
+      .addField("model", hostinfo.cpu.model || "???", true)
+      .addField("stepping", hostinfo.cpu.stepping || "???", true)
+      .addField("base clock", hostinfo.cpu.speed + " GHz" || "???", true)
+      .addField("min speed", hostinfo.cpu.speedMin + " GHz" || "???", true)
+      .addField("max speed", hostinfo.cpu.speedMax + " GHz" || "???", true)
+      .addField(
+        "num of CPUs",
+        hostinfo.cpu.processors.toString() || "???",
+        true
+      )
+      .addField("cores", hostinfo.cpu.physicalCores.toString() || "???", true)
+      .addField("threads", hostinfo.cpu.cores.toString() || "???", true)
+      .addField(
+        "supported features",
+        "```\n" + hostinfo.cpu.flags.replaceAll(" ", ", ") + "\n```" || "???"
+      );
+
     const mem = new MessageEmbed()
       .setTitle(`📋 memory`)
       .setColor(cfg.embed.colours.default)
-      .addField("total memory", hostinfo.mem.info.total / 1073741824 + " gigabytes", true)
-      .addField("type", hostinfo.mem.layout[0].type, true)
-      .addField("clock speed", hostinfo.mem.layout[0].clockSpeed + " MHz", true)
-      .addField("error-correcting", hostinfo.mem.layout[0].ecc.toString().replace(true, "yes").replace(false, "no"), true)
+      .addField(
+        "total memory",
+        hostinfo.mem.info.total / 1073741824 + " gigabytes" || "???",
+        true
+      )
+      .addField("type", hostinfo.mem.layout[0].type || "???", true)
+      .addField(
+        "clock speed",
+        hostinfo.mem.layout[0].clockSpeed + " MHz" || "???",
+        true
+      )
+      .addField(
+        "error-correcting",
+        hostinfo.mem.layout[0].ecc
+          .toString()
+          .replace(true, "yes")
+          .replace(false, "no"),
+        true
+      )
       .addField("form-factor", hostinfo.mem.layout[0].formFactor || "???", true)
-      .addField("slot", hostinfo.mem.layout[0].bank, true)
-      .addField("voltage", hostinfo.mem.layout[0].voltageConfigured || "???", true)
+      .addField("slot", hostinfo.mem.layout[0].bank || "???", true)
+      .addField(
+        "voltage",
+        hostinfo.mem.layout[0].voltageConfigured || "???",
+        true
+      )
       .addField("min voltage", hostinfo.mem.layout[0].voltageMin || "???", true)
-      .addField("max voltage", hostinfo.mem.layout[0].voltageMax || "???", true)
-    
+      .addField(
+        "max voltage",
+        hostinfo.mem.layout[0].voltageMax || "???",
+        true
+      );
+
     const gfx = new MessageEmbed()
       .setTitle(`👾 graphics`)
       .setColor(cfg.embed.colours.default)
-      .addField("vendor", hostinfo.gfx.controllers[0].vendor, true)
-      .addField("model", hostinfo.gfx.controllers[0].model, true)
-      .addField("connection", hostinfo.gfx.controllers[0].bus, true)
-      .addField("dynamic vram", hostinfo.gfx.controllers[0].vramDynamic.toString().replace("true", "yes").replace("false", "no"), true)
-      .addField("video memory", hostinfo.gfx.controllers[0].vram + " MB", true)
-      .addField("metal version", hostinfo.gfx.controllers[0].metalVersion || "not on macOS", true)
-    
+      .addField("vendor", hostinfo.gfx.controllers[0].vendor || "???", true)
+      .addField("model", hostinfo.gfx.controllers[0].model || "???", true)
+      .addField("connection", hostinfo.gfx.controllers[0].bus || "???", true)
+      .addField(
+        "dynamic vram",
+        hostinfo.gfx.controllers[0].vramDynamic
+          .toString()
+          .replace("true", "yes")
+          .replace("false", "no") || "???",
+        true
+      )
+      .addField(
+        "video memory",
+        hostinfo.gfx.controllers[0].vram || "???" + " MB",
+        true
+      )
+      .addField(
+        "metal version",
+        hostinfo.gfx.controllers[0].metalVersion || "not on macOS",
+        true
+      );
+
     const ops = new MessageEmbed()
       .setTitle(`🌱 operating system`)
       .setColor(cfg.embed.colours.default)
-      .addField("distribution", hostinfo.os.sys.distro, true)
-      .addField("release", hostinfo.os.sys.release, true)
-      .addField("codename", hostinfo.os.sys.codename.replace('macOS ', '').replace('Mac OS X ', '') || "???", true)
-      .addField("platform", hostinfo.os.sys.platform, true)
+      .addField("distribution", hostinfo.os.sys.distro || "???", true)
+      .addField("release", hostinfo.os.sys.release || "???", true)
+      .addField(
+        "codename",
+        hostinfo.os.sys.codename
+          .replace("macOS ", "")
+          .replace("Mac OS X ", "") || "???",
+        true
+      )
+      .addField("platform", hostinfo.os.sys.platform || "???", true)
       .addField("build", hostinfo.os.sys.build || "???", true)
-      .addField("kernel version", hostinfo.os.sys.kernel, true)
-      .addField("fqdn", hostinfo.os.sys.fqdn, true)
-      .addField("boot mode", bootMode(), true)
+      .addField("kernel version", hostinfo.os.sys.kernel || "???", true)
+      .addField("fqdn", hostinfo.os.sys.fqdn || "???", true)
+      .addField("boot mode", bootMode() || "???", true)
       .addField("default shell", hostinfo.os.shell || "???", true)
-      .addField("service pack", hostinfo.os.sys.servicepack || "not on Windows", true)
-      .addField("under hyper-v", hostinfo.os.sys.hypervizor || "not on Windows", true)
-      .addField("through rdp", hostinfo.os.sys.remoteSession || "not on Windows", true)
-    
+      .addField(
+        "service pack",
+        hostinfo.os.sys.servicepack || "not on Windows",
+        true
+      )
+      .addField(
+        "under hyper-v",
+        hostinfo.os.sys.hypervizor || "not on Windows",
+        true
+      )
+      .addField(
+        "through rdp",
+        hostinfo.os.sys.remoteSession || "not on Windows",
+        true
+      );
+
     const sw = new MessageEmbed()
       .setTitle(`🌸 software`)
-      .setColor(cfg.embed.colours.default)
-    
+      .setColor(cfg.embed.colours.default);
+
     Object.entries(hostinfo.os.ver).forEach(([k, v]) => {
-      if (!v || k === 'kernel') return;
-      sw.addField(k, v, true)
-    })
+      if (!v || k === "kernel") return;
+      sw.addField(k, v, true);
+    });
 
     const disks = new MessageEmbed()
       .setTitle(`👢 boot drive`)
       .setColor(cfg.embed.colours.default)
       .addField("name", hostinfo.disk[0].name || "???", true)
-      .addField("vendor", hostinfo.disk[0].vendor, true)
-      .addField("size", Math.floor(hostinfo.disk[0].size / 1073741824) + " GB" || "???", true)
+      .addField("vendor", hostinfo.disk[0].vendor || "???", true)
+      .addField(
+        "size",
+        Math.floor(hostinfo.disk[0].size / 1073741824) + " GB" || "???",
+        true
+      )
       .addField("device", hostinfo.disk[0].device || "???", true)
-      .addField("type", hostinfo.disk[0].type.replace('HD', 'Hard Disk Drive').replace('SSD', 'Solid State Drive').replace('NVMe', 'Solid State Drive over NVMe'), true)
+      .addField(
+        "type",
+        hostinfo.disk[0].type
+          .replace("HD", "Hard Disk Drive")
+          .replace("SSD", "Solid State Drive")
+          .replace("NVMe", "Solid State Drive over NVMe") || "???",
+        true
+      )
       .addField("interface", hostinfo.disk[0].interfaceType || "???", true)
       .addField("firmware", hostinfo.disk[0].firmwareRevision || "???", true)
       .addField("temperature", hostinfo.disk[0].temperature || "-1", true)
-      .addField("S.M.A.R.T. status", hostinfo.disk[0].smartStatus.replace('unknown', '???') || "???", true)
-    
-      const usb = new MessageEmbed()
-        .setTitle(`🍆 usb controller`)
-        .setColor(cfg.embed.colours.default)
-        .addField("name", hostinfo.usb[0].name || "???", true)
-        .addField("type", hostinfo.usb[0].type, true)
-        .addField("removable", hostinfo.usb[0].removable.toString().replace("true", "yes").replace("false", "no"), true)
-        .addField("vendor", hostinfo.usb[0].vendor, true)
-        .addField("manufacturer", hostinfo.usb[0].manufacturer, true)
-        .addField("max power", hostinfo.usb[0].maxPower || '???', true)
+      .addField(
+        "S.M.A.R.T. status",
+        hostinfo.disk[0].smartStatus.replace("unknown", "???") || "???",
+        true
+      );
+
+    const usb = new MessageEmbed()
+      .setTitle(`🍆 usb controller`)
+      .setColor(cfg.embed.colours.default)
+      .addField("name", hostinfo.usb[0].name || "???", true)
+      .addField("type", hostinfo.usb[0].type || "???", true)
+      .addField(
+        "removable",
+        hostinfo.usb[0].removable
+          .toString()
+          .replace("true", "yes")
+          .replace("false", "no") || "???",
+        true
+      )
+      .addField("vendor", hostinfo.usb[0].vendor || "???", true)
+      .addField("manufacturer", hostinfo.usb[0].manufacturer || "???", true)
+      .addField("max power", hostinfo.usb[0].maxPower || "???", true);
     // #endregion
 
     if (!opt) return interaction.reply({ embeds: [general, host] });
     if (opt === "general") return interaction.reply({ embeds: [general] });
     if (opt === "host") return interaction.reply({ embeds: [host] });
-    if (opt === "nerd") return interaction.reply({ embeds: [board, cpu, mem, gfx, ops, sw, disks, usb] });
+    if (opt === "nerd")
+      return interaction.reply({
+        embeds: [board, cpu, mem, gfx, ops, sw, disks, usb],
+      });
 
     interaction.reply({
       content: "you specified an invalid detail level!",
