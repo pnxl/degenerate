43717e7591a7fa86faedad7e73399377f4aebe02
Jason <pix3l.icious@gmail.com>
2022-06-14 22:18:21 +0700

    .includes exists?!

--- src/commands/development/evaluate.js
+++ src/commands/development/evaluate.js
@@ -16,41 +16,7 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    const code = interaction.options.getString("code");
-
-    if (
-      interaction.user.id === cfg.info.owner.id[0] ||
-      interaction.user.id === cfg.info.owner.id[1]
-    ) {
-      // actual eval code
-      print.warn(
-        `'${interaction.user.tag}' ran evaluate at <#${interaction.channel.id}> and ran:\n            ${code}`
-      );
-
-      let res;
-      try {
-        res = eval(code);
-        res = inspect(res, { depth: 0 });
-      } catch (error) {
-        res = inspect(error, { depth: 0 });
-      }
-
-      const embed = new MessageEmbed()
-        .setAuthor({
-          name: interaction.user.tag,
-          iconURL: interaction.user.displayAvatarURL(),
-        })
-        .setColor(cfg.embed.colour)
-        .setTitle("evaluation")
-        .addField("code snippet", `\`\`\`js\n${code.slice(0, 1015)}\`\`\``)
-        .addField("node.js results", `\`\`\`js\n${res.slice(0, 1015)}\`\`\``)
-        .setFooter({
-          text: `running node.js ${process.version}`,
-        })
-        .setTimestamp();
-
-      interaction.reply({ embeds: [embed], ephemeral: true });
-    } else {
+    if (cfg.info.owner.id.includes(interaction.user.id) === false) {
       interaction.reply({
         content: "you do not have permission to use that command!",
         ephemeral: true,
@@ -59,5 +25,36 @@ module.exports = {
         "A user attempted to run evaluate! Make sure to remove evaluate before getting ready for production!"
       );
     }
+
+    const code = interaction.options.getString("code");
+
+    // actual eval code
+    print.warn(
+      `'${interaction.user.tag}' ran evaluate at <#${interaction.channel.id}> and ran:\n            ${code}`
+    );
+
+    let res;
+    try {
+      res = eval(code);
+      res = inspect(res, { depth: 0 });
+    } catch (error) {
+      res = inspect(error, { depth: 0 });
+    }
+
+    const embed = new MessageEmbed()
+      .setAuthor({
+        name: interaction.user.tag,
+        iconURL: interaction.user.displayAvatarURL(),
+      })
+      .setColor(cfg.embed.colour)
+      .setTitle("evaluation")
+      .addField("code snippet", `\`\`\`js\n${code.slice(0, 1015)}\`\`\``)
+      .addField("node.js results", `\`\`\`js\n${res.slice(0, 1015)}\`\`\``)
+      .setFooter({
+        text: `running node.js ${process.version}`,
+      })
+      .setTimestamp();
+
+    interaction.reply({ embeds: [embed], ephemeral: true });
   },
 };
